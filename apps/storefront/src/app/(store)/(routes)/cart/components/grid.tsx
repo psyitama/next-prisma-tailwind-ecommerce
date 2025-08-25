1e8f7d9e1bc25936e9c330e82fac99403e0936d8
psyitama <psyrone.agpalo@gmail.com>
2025-08-25 20:34:31 +0800

    feat(storefront/cart): add related products you may like view

--- apps/storefront/src/app/(store)/(routes)/cart/components/grid.tsx
+++ apps/storefront/src/app/(store)/(routes)/cart/components/grid.tsx
@@ -7,6 +7,8 @@ import { useCartContext } from '@/state/Cart'
 import { Item } from './item'
 import { Receipt } from './receipt'
 import { Skeleton } from './skeleton'
+import { RelatedProducts } from "../../products/[productId]/components/related_product"
+import { Separator } from "@/components/native/separator"
 
 export const CartGrid = () => {
    const { loading, cart, refreshCart, dispatchCart } = useCartContext()
@@ -26,18 +28,41 @@ export const CartGrid = () => {
       )
    }
 
+   const relatedProducts = () => {
+      const mergedCrossSellProducts = []
+
+      if (isVariableValid(cart?.items) && cart?.items?.length) {
+         return [
+            ...new Map(
+               cart.items
+                  .flatMap(item => item?.product?.crossSellProducts ?? [])
+                  .map(product => [product.id, product]) // use id as key
+            ).values(),
+         ]
+      }
+
+      return mergedCrossSellProducts
+   }
+
    return (
-      <div className="mb-4 grid grid-cols-1 md:grid-cols-3 gap-3">
-         <div className="md:col-span-2">
-            {isVariableValid(cart?.items)
-               ? cart?.items?.map((cartItem, index) => (
-                    <Item cartItem={cartItem} key={index} />
-                 ))
-               : [...Array(5)].map((cartItem, index) => (
-                    <Skeleton key={index} />
-                 ))}
+      <>
+         <div className="mb-4 grid grid-cols-1 md:grid-cols-3 gap-3">
+            <div className="md:col-span-2">
+               {isVariableValid(cart?.items)
+                  ? cart?.items?.map((cartItem, index) => (
+                     <Item cartItem={cartItem} key={index} />
+                  ))
+                  : [...Array(5)].map((cartItem, index) => (
+                     <Skeleton key={index} />
+                  ))}
+            </div>
+            <Receipt />
          </div>
-         <Receipt />
-      </div>
+         <Separator />
+         <div>
+            <h2 className="mb-4 text-xl font-bold tracking-tight">You might also like</h2>
+            <RelatedProducts products={relatedProducts()} />
+         </div>
+      </>
    )
 }
