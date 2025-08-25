d377851c3b696fd68ad63670ed502d4d75ac970c
psyitama <psyrone.agpalo@gmail.com>
2025-08-25 15:20:18 +0800

    feat(admin/reports): add page and nav

---
+++ apps/admin/src/app/(dashboard)/(routes)/admin/reports/page.tsx
@@ -0,0 +1,121 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Heading } from '@/components/ui/heading'
+import { Separator } from '@/components/ui/separator'
+import prisma from '@/lib/prisma'
+import { formatter } from '@/lib/utils'
+import { CreditCard, DollarSign, Package } from 'lucide-react'
+
+import { OrderReportChart } from './components/chart'
+import { ProductColumn, ProductsTable } from './components/table'
+import { format } from "date-fns"
+import { BrandCombobox, CategoriesCombobox, DateRangePicker } from "./components/options"
+
+const startDate = new Date('2023-10-27')
+const endDate = new Date('2025-08-25')
+
+const brands = await prisma.brand.findMany()
+const categories = await prisma.category.findMany()
+
+// Charts data
+const orders = await prisma.order.groupBy({
+    by: ['createdAt'],
+    _count: {
+        id: true, // count total orders
+    },
+    where: {
+        createdAt: {
+            gte: startDate,
+            lte: endDate,
+        }
+    },
+});
+
+const groupedOrders: Record<string, number> = {};
+
+// Aggregate orders by date using date-fns for formatting
+orders.forEach(order => {
+    const date = format(order.createdAt, 'yyyy-MM-dd'); // format date
+    groupedOrders[date] = (groupedOrders[date] || 0) + order._count.id;
+});
+
+// Format for chart
+const ordersChartData = Object.entries(groupedOrders)
+  .map(([date, orderCount]) => ({ date, orderCount }))
+  .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+
+
+// TABLE DATA: Top Selling Products
+const products = await prisma.product.findMany({
+    select: {
+        id: true,
+        title: true,
+        price: true,
+        discount: true,
+        isAvailable: true,
+        categories: { select: { title: true } },
+        orders: {
+            where: {
+                order: {
+                    createdAt: { gte: startDate, lte: endDate },
+                },
+            },
+            select: {
+                orderId: true,
+                count: true,
+                order: {
+                    select: {
+                        createdAt: true
+                    }
+                }
+            },
+        },
+    },
+})
+
+const topSellingProducts: ProductColumn[] = products.map((product) => ({
+   id: product.id,
+   title: product.title,
+   price: formatter.format(product.price),
+   discount: formatter.format(product.discount),
+   category: product.categories[0].title,
+   sales: product.orders.length,
+   isAvailable: product.isAvailable,
+}))
+
+export default async function ReportsPage({ searchParams }) {
+   const {  startDate, endDate, brand, category, page = 1 } = searchParams ?? null
+
+   return (
+      <div className="block my-6 space-y-4">
+         <Heading title={'Reports'} description="Product and order insights" />
+         <Separator />
+         <div className="grid gap-4 grid-cols-3">
+            <DateRangePicker
+               initialStartDate={startDate}
+               initialEndDate={endDate}
+            />
+            <CategoriesCombobox
+               initialCategory={category}
+               categories={categories}
+            />
+            <BrandCombobox initialBrand={brand} brands={brands} />
+         </div>
+         <Card className="col-span-4">
+            <CardHeader>
+               <CardTitle>Orders</CardTitle>
+            </CardHeader>
+            <CardContent className="pl-2">
+               <OrderReportChart data={ordersChartData} />
+            </CardContent>
+         </Card>
+         <Card className="col-span-4">
+            <CardHeader>
+               <CardTitle>Top Selling Products</CardTitle>
+            </CardHeader>
+            <CardContent className="pl-2">
+               <ProductsTable data={topSellingProducts} />
+            </CardContent>
+         </Card>
+      </div>
+   )
+}
