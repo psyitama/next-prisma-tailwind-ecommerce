4ba9cf7eb9fb06fc7325c6e84dd18f164f5b3235
psyitama <psyrone.agpalo@gmail.com>
2025-08-25 15:19:44 +0800

    feat(admin/reports/ui): add chart component

---
+++ apps/admin/src/app/(dashboard)/(routes)/admin/reports/components/chart.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
+
+interface OrderReportProps {
+   data: any[]
+}
+
+export const OrderReportChart: React.FC<OrderReportProps> = ({ data }) => {
+    console.log('OrderReportChart', data)
+   return (
+    <ResponsiveContainer width="100%" height={350}>
+        <LineChart data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="orderCount" stroke="#3498db" />
+        </LineChart>
+    </ResponsiveContainer>
+   )
+}
