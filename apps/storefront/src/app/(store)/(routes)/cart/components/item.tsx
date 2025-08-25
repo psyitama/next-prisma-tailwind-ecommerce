c673cc4fbf8722398ad1df48f1fa7ab772e51e8a
psyitama <psyrone.agpalo@gmail.com>
2025-08-25 20:48:36 +0800

    fix(storefront/cart): wrong item was being removed from cart

--- apps/storefront/src/app/(store)/(routes)/cart/components/item.tsx
+++ apps/storefront/src/app/(store)/(routes)/cart/components/item.tsx
@@ -26,12 +26,15 @@ export const Item = ({ cartItem }) => {
 
    const { product, productId, count } = cartItem
 
-   function findLocalCartIndexById(array, productId) {
-      for (let i = 0; i < array.length; i++) {
-         if (array?.items[i]?.productId === productId) {
+   function findLocalCartIndexById(cart, productId) {
+      const cart_items = cart?.items || [];
+
+      for (let i = 0; i < cart_items.length; i++) {
+         if (cart_items[i]?.productId === productId) {
             return i
          }
       }
+
       return -1
    }
 
